8f625e8250e6a16d3593e960252fd9c83d99fbdb
pisethsok061021@gmail.com <pisethsok061021@gmail.com>
2024-12-21 23:38:38 +0700

    add [word generation, typing speed, loading component, and more]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,73 @@
 import Button from "./Button/Button.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [words, setWords] = useState("25");
+  const [arrayword, setArrayWord] = useState([]);
+  const [wpm, setWpm] = useState(0);
+  let [loading, setLoading] = useState(false);
+
+  function handleRadio(e) {
+    setWords(e.target.value);
+    document.querySelector("#typeInput").focus();
+    const radios = document.querySelectorAll(".labelRadio");
+    radios.forEach((element) => (element.className = "labelRadio"));
+    e.target.parentElement.className += " radio-bg";
+  }
+
+  useEffect(() => {
+    let mounted = true;
+    setLoading(true);
+
+    const fetchWords = async () => {
+      try {
+        const response = await fetch(`https://random-word-api.herokuapp.com/word?number=${words}`);
+        const data = await response.json();
+        if (mounted) {
+          setArrayWord(data);
+        }
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching words:", error);
+      }
+    };
+
+    fetchWords();
+
+    return () => {
+      mounted = false;
+    };
+  }, [words]);
+
   return (
     <>
       <h1>Have Fun Typing!</h1>
-      <div id="container">
-        <Button />
+      <div className="container hight-30">
+        <div className="Panel">
+          <div className="wordContContainer">
+            <label htmlFor="twentyFive" className="labelRadio radio-bg">
+              <input type="radio" id="twentyFive" className="Radio" value="25" checked={words == "25"} onChange={handleRadio} />
+              25
+            </label>
+            <label htmlFor="Fifty" className="labelRadio">
+              <input type="radio" id="Fifty" className="Radio" value="50" checked={words == "50"} onChange={handleRadio} />
+              50
+            </label>
+            <label htmlFor="oneHundred" className="labelRadio">
+              <input type="radio" id="oneHundred" className="Radio" value="100" checked={words == "100"} onChange={handleRadio} />
+              100
+            </label>
+            <label htmlFor="oneFifty" className="labelRadio">
+              <input type="radio" id="oneFifty" className="Radio" value="150" checked={words == "150"} onChange={handleRadio} />
+              150
+            </label>
+          </div>
+          <p>WPM: {wpm}</p>
+        </div>
+      </div>
+
+      <div className="container">
+        <Button word={arrayword} wpm={setWpm} setword={setWords} loading={loading} setLoading={setLoading} />
       </div>
     </>
   );
--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,5 +1,6 @@
 import style from "./Button.module.css";
-function Button() {
+import RiseLoader from "react-spinners/RiseLoader";
+function Button(props) {
   const keyboardLayout = [
     ["`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace"],
     ["Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\"],
@@ -10,44 +11,44 @@ function Button() {
 
   const firstrow = keyboardLayout[0].map((element) =>
     element == "Backspace" ? (
-      <button key={element} className={`${style.firstbutton} ${style.mediumBtn}`} id={element} onClick={(e) => targetClick(e)}>
+      <button key={element} className={`${style.firstbutton} ${style.mediumBtn}`} id={element}>
         {element}
       </button>
     ) : (
-      <button key={element} className={style.firstbutton} id={element.toLowerCase()} onClick={(e) => targetClick(e)}>
+      <button key={element} className={style.firstbutton} id={element.toLowerCase()}>
         {element}
       </button>
     )
   );
   const secondrow = keyboardLayout[1].map((element) =>
     element == "Tab" ? (
-      <button key={element} className={`${style.firstbutton} ${style.mediumBtn}`} id={element} onClick={(e) => targetClick(e)}>
+      <button key={element} className={`${style.firstbutton} ${style.mediumBtn}`} id={element}>
         {element}
       </button>
     ) : (
-      <button key={element} className={style.firstbutton} id={element.toLowerCase()} onClick={(e) => targetClick(e)}>
+      <button key={element} className={style.firstbutton} id={element.toLowerCase()}>
         {element}
       </button>
     )
   );
   const thirdrow = keyboardLayout[2].map((element) =>
     element == "Enter" || element == "CapsLock" ? (
-      <button key={element} className={`${style.firstbutton} ${style.enterBtn}`} id={element} onClick={(e) => targetClick(e)}>
+      <button key={element} className={`${style.firstbutton} ${style.enterBtn}`} id={element}>
         {element}
       </button>
     ) : (
-      <button key={element} className={style.firstbutton} id={element.toLowerCase()} onClick={(e) => targetClick(e)}>
+      <button key={element} className={style.firstbutton} id={element.toLowerCase()}>
         {element}
       </button>
     )
   );
   const fourhtrow = keyboardLayout[3].map((element) =>
     element == "LShift" || element == "Shift" ? (
-      <button key={element} className={`${style.firstbutton} ${style.shiftBtn}`} id={element} onClick={(e) => targetClick(e)}>
+      <button key={element} className={`${style.firstbutton} ${style.shiftBtn}`} id={element}>
         {element}
       </button>
     ) : (
-      <button key={element} className={style.firstbutton} id={element.toLowerCase()} onClick={(e) => targetClick(e)}>
+      <button key={element} className={style.firstbutton} id={element.toLowerCase()}>
         {element}
       </button>
     )
@@ -55,25 +56,30 @@ function Button() {
   const fiftrow = keyboardLayout[4].map((element) => {
     if (element === "Space") {
       return (
-        <button key={element} className={`${style.firstbutton} ${style.spaceBtn}`} id={element} onClick={(e) => targetClick(e)}>
+        <button key={element} className={`${style.firstbutton} ${style.spaceBtn}`} id={element}>
           {element}
         </button>
       );
     } else if (element === "LCtrl" || element === "Ctrl") {
       return (
-        <button key={element} className={`${style.firstbutton} ${style.enterBtn}`} id={element} onClick={(e) => targetClick(e)}>
+        <button key={element} className={`${style.firstbutton} ${style.enterBtn}`} id={element}>
           {element}
         </button>
       );
     }
     return (
-      <button key={element} className={style.firstbutton} id={element} onClick={() => targetClick()}>
+      <button key={element} className={style.firstbutton} id={element}>
         {element}
       </button>
     );
   });
-
+  let newtem = "";
+  let begin;
   const test = (event) => {
+    if (!begin) {
+      begin = performance.now();
+    }
+    const targetValueInput = document.querySelector("#typeWords");
     const keyIdentify = event.key;
     let sanitizedKey = keyIdentify.replace(/([.*+?^=!:${}()`|\[\]\/\\-])/g, "\\$1");
     sanitizedKey == " " ? (sanitizedKey = "Space") : sanitizedKey;
@@ -83,8 +89,6 @@ function Button() {
       const whitL = "L" + sanitizedKey;
       const target = document.querySelector(`#${sanitizedKey}`);
       const whitLTarget = document.querySelector(`#${whitL}`);
-
-      target.click();
       setTimeout(() => {
         target.className += ` ${style.newbutton}`;
         whitLTarget.className += ` ${style.newbutton}`;
@@ -103,13 +107,68 @@ function Button() {
         target.classList.remove(style.newbutton);
       }
     }
+
+    // typing logic
+    if (keyIdentify == targetValueInput.value.charAt(0)) {
+      let temval = targetValueInput.value;
+      targetValueInput.value = temval.slice(1);
+      newtem += keyIdentify;
+    } else if (keyIdentify == "Backspace") {
+      targetValueInput.value = newtem.charAt(newtem.length - 1) + targetValueInput.value;
+      newtem = newtem.slice(0, newtem.length - 1);
+    } else if (targetValueInput.value.length == 0) {
+      const end = performance.now() - begin;
+      const endMinute = end / (1000 * 60);
+      console.log(event.target.value.length, endMinute);
+      props.wpm((event.target.value.length / 5 / endMinute).toFixed(2));
+      event.target.value = "";
+      begin = 0;
+      console.log(props.word.length);
+      switch (props.word.length) {
+        case 25:
+          props.setword("1");
+          setTimeout(() => {
+            props.setword("25");
+          }, 0);
+          break;
+        case 50:
+          props.setword("1");
+          setTimeout(() => {
+            props.setword("50");
+          }, 0);
+          break;
+        case 100:
+          props.setword("1");
+          setTimeout(() => {
+            props.setword("100");
+          }, 0);
+          break;
+        case 150:
+          props.setword("1");
+          setTimeout(() => {
+            props.setword("150");
+          }, 0);
+          break;
+      }
+    } else {
+      setTimeout(() => {
+        document.querySelector("#typeInput").value = "";
+        document.querySelector("#typeInput").value = newtem;
+      }, 0);
+    }
   };
-  const targetClick = (e) => {
-    console.log(`${e.target.textContent} am click`);
-  };
+
   return (
     <div className={style.keyboard}>
-      <input autoFocus type="text" name="" className={style.textToType} onKeyDown={(event) => test(event)} />
+      {props.loading ? (
+        <RiseLoader loading={props.loading} size={25} aria-label="Loading Spinner" data-testid="loader" />
+      ) : (
+        <div>
+          <input autoFocus type="text" id="typeInput" className={style.textToType} onKeyDown={(event) => test(event)} autoComplete="off" />
+          <input disabled type="text" id="typeWords" className={`${style.textToType} ${style.textForType}`} value={props.word.join(" ")} />
+        </div>
+      )}
+
       <br />
       <br />
       <div>{firstrow}</div>
